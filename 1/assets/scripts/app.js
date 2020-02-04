3de0c2dc9feb517a6e7f4dd57f08a9e8a73b6e50
Diego G <diego.gomes87@gmail.com>
2020-02-04 07:59:48 -0300

    update css and working in app js

--- 1/assets/scripts/app.js
+++ 1/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert("here this function")
+let currentResult = 0 
+
+currentResult = (currentResult + 10 ) * 3 /2 -1
+
+let calculationDescription = `Hey`
+
+outResult(currentResult, calculationDescription)
--- 1/assets/scripts/vendor.js
+++ 1/assets/scripts/vendor.js
@@ -4,7 +4,7 @@ const subtractBtn = document.getElementById('btn-subtract')
 const multiplyBtn = document.getElementById('btn-multiply')
 const divideBtn = document.getElementById('btn-divide')
 const currentResultOutput = document.getElementById('current-result')
-const currentCalculationOutput = document.getElementById('current-calculation')
+const currentCalculationOutput = document.getElementById('current-calculator')
 
 function outResult(result, text){
   currentResultOutput.textContent = result
