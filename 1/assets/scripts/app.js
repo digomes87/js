a5a6cad4c6c10fa05d46d5b3d936bf282d590538
Diego G <diego.gomes87@gmail.com>
2020-02-17 13:48:27 -0300

    new project js

--- 1/assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-const defaultResult = 0
-let currentResult = defaultResult
-
-currentResult = (defaultResult + 10 ) * 3 /2 -1
-
-let calculationDescription = `(${defaultResult} + 10 ) * 3 /2 -1`
-
-outResult(currentResult, calculationDescription)
--- 2/assets/main.js
+++
@@ -1,8 +0,0 @@
-function frase(){
-  let frase = "Japão é o melhor time do mundo"
-
-  //mundando parte da frase
-  frase = frase.replace("Japão", "Brasil")
-  document.body.innerHTML = frase
-
-}
